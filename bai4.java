b50b5c3ebbafdbf0b977bd9f0f05a69e07fd282f
phatba777-cmyk <phatba777@gmail.com>
2025-09-17 08:35:29 +0700

    commitlan1

---
+++ bai4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap n: ");
+        int n = sc.nextInt();
+
+        int tong = 0;
+        for (int i = 1; i <= n; i++) {
+            tong += i;
+        }
+
+        System.out.println("Tong cac so tu 1 den " + n + " la: " + tong);
+        sc.close();
+    }
+}
+
